91b62d550987a6c76eef84739a57d0d6d7568109
przekorny <39132868+przekorny@users.noreply.github.com>
2020-05-12 15:56:25 +0200

    Add files via upload

--- ProjektAdaśSzymon.R
+++ ProjektAdaśSzymon.R
@@ -93,10 +93,11 @@ USbond <- read.csv2(file="USbond.csv",sep = ";")
 USbond <- USbond[,1:2]
 USbond[,1] <- as.Date(USbond[,1],"%d.%m.%Y")
 colnames(USbond) <- c("data","zamkniecie")
-USbond <- USbond[nrow(USbond):1,]    # notowana jest rentownosc obligacji. nie ich cena. Szczeg?ly w pdf.
+USbond <- USbond[nrow(USbond):1,]    # notowana jest rentownosc obligacji. nie ich cena. Szczególy w pdf.
 databond <- sapply(datess, function(x) sum(USbond[,1] < x) + 1)  # powtarzamy podzielenie na miesiace wedlug daty
 databond[37] <- 919  # problem taki sam jak z notowaniami
-stopa_all <- (USbond[databond,2]*(1/12))/100 # rentownosc obligacji 1 dnia kazdego miesiaca, w skali miesiecznej, dzielimy przez 100 poniewaz jest wynik procentowy
+stopa_all <- ((1 +(USbond[databond,2])/100)^(1/12) -1) # rentownosc obligacji 1 dnia kazdego miesiaca, w skali miesiecznej, dzielimy przez 100 poniewaz jest wynik procentowy
+#stopa_all <- (USbond[databond,2]*(1/12))/100
 
 sp <- read.csv2(file="S&P500.csv", sep = ",")
 sp <- sp[,c(1,5)]
@@ -106,41 +107,68 @@ spdsz <- sp[-1,2]/sp[-dim(sp)[1],2] -1
 gielda_all <- sp[dates[-1],2]/sp[dates[-length(dates)],2] -1  #miesieczne stopy zwrotu z indeksu S&P 500
 stopyzwrotu <- matrix(c(portfel_ret_all,gielda_all[13:length(gielda_all)],stopa_all[13:(length(stopa_all)-1)]),ncol=4,dimnames = list(NULL,c("portfelX","portfelY","gielda_all","stopa_all")))
 #dostajemy macierz z miesiecznymi stopami zwrotu z porfela x,Y,S&P500 i inwestujac w obligacje dzisiecioletnie w USA, tutaj bierzemy poczatek kazdej miesiecznej inwestycji
-odchs <- c()
-for(i in 1:24){
-  odchs[i] <-sd(gielda_all[i:(11+i)])  
-}   #policzylismy odchylenia z miesiecznych st?p zwrotu dla poszczeg?lnych okres?w, w kt?rych inwestujemy.
-
-sharpe_all <- matrix(ncol=3,nrow = 24,dimnames = list(NULL,c("portfelX","portfelY","S&P500")))
+odchs <-sd(gielda_all[13:36])  
+#policzylismy odchylenia z miesiecznych rynkowych stóp zwrotu dla okresu, w których inwestujemy.
+sharpe_all <- matrix(ncol=3,nrow = 1,dimnames = list(NULL,c("portfelX","portfelY","S&P500")))
 for(i in 1:3){
-  sharpe_all[,i] <- (stopyzwrotu[,i]-stopyzwrotu[,4])/odchs  
+  sharpe_all[,i] <- (mean(stopyzwrotu[,i])-mean(stopyzwrotu[,4]))/odchs  
 } # policzylismy sharpe_ratio dla portfeli X,Y i S&P500
 beta_allX <- c()
 beta_allY <- c()
 alfa_allX <- c()
 alfa_allY <- c()
-for(i in 1:12){
-  CAPM <- lm((stopyzwrotu[i:(11+i),1]-stopyzwrotu[i:(11+i),4])~(stopyzwrotu[i:(11+i),3]-stopyzwrotu[i:(11+i),4]))
-  CAPM2 <- lm((stopyzwrotu[i:(11+i),2]-stopyzwrotu[i:(11+i),4])~(stopyzwrotu[i:(11+i),3]-stopyzwrotu[i:(11+i),4]))
+for(i in 1:24){
+  CAPM <- lm((rowSums(return_all[i:(11+i),]*wagi_all[i,])-stopa_all[i:(11+i)])~(gielda_all[i:(11+i)]-stopa_all[i:(11+i)]))
+  CAPM2 <- lm((rowSums(return_all[i:(11+i),]*(1/8))-stopa_all[i:(11+i)])~(gielda_all[i:(11+i)]-stopa_all[i:(11+i)]))
   alfa_allX[i] <- CAPM$coefficients[1]
   beta_allX[i] <- CAPM$coefficients[2]
   alfa_allY[i] <- CAPM2$coefficients[1]
   beta_allY[i] <- CAPM2$coefficients[2]
-}
+} # dla modelu CAPM liczymy bety i alfy
 alfa_all <- matrix(c(alfa_allX,alfa_allY),ncol=2,dimnames = list(NULL,c("alfa_allX","alfa_allY")))
 beta_all <- matrix(c(beta_allX,beta_allY),ncol=2,dimnames = list(NULL,c("beta_allX","beta_allY")))
+result_CAPM <- matrix(ncol=2,nrow = 24)
+result_CAPM <- alfa_all + beta_all*(stopyzwrotu[,3]-stopyzwrotu[,4])  # przewidywany wynik wedlug modelu CAPM
+plot(result_CAPM[,1],type = "l")
+lines(stopyzwrotu[,1], col="blue")
+plot(result_CAPM[,2],type = "l")
+lines(stopyzwrotu[,2], col="blue")
+# czy moze lepiej to zilustrowac w formie tabelki/ albo w ogóle np. bledów?
+# dodatkowe wykresy i wskazniki
+# policzymy drawdown
+portfelSP500 <- 10000
+portfelbonds <- 10000
+for(i in 2:25){
+  portfelSP500[i] <-  portfelSP500[(i-1)]*(1+stopyzwrotu[(i-1),3])
+  portfelbonds[i] <-  portfelbonds[(i-1)]*(1+stopyzwrotu[(i-1),4])
+}
+DrowdawnsX <- c()
+DrowdawnsY <- c()
+DrowdawnsSP500 <- c()
+Drowdawnsbonds <- c()
+for(i in 1:24){
+  DrowdawnsX[i] <- max(portfelX[i]-portfelX[(i+1):25])
+  DrowdawnsY[i] <- max(portfelY[i]-portfelY[(i+1):25])
+  DrowdawnsSP500[i] <- max(portfelSP500[i]-portfelSP500[(i+1):25])
+  Drowdawnsbonds[i] <- max(portfelbonds[i]-portfelbonds[(i+1):25])
+}
+Drowdawn <- apply(matrix(c(DrowdawnsX,DrowdawnsY,DrowdawnsSP500,Drowdawnsbonds),ncol=4),2,max)
+# wykres zysków portfela X i Y, S&P500 i obligacji
+plot(portfelX,type="l",col=1,xlab = "kapital w kolejnych miesiacach inwestycji",ylab = "wartosc portfela")
+lines(portfelY,col=2)
+lines(portfelSP500,col=3)
+lines(portfelbonds,col=4)
+legend(1,15500,legend=c("porfelX", "porfelY","portfelSP500","portfelbonds"),
+       col=c(1,2,3,4), lty=1, cex=0.7)
+a
 
-# to jest na razie zle we wtorek sie dowiem jak to poprawic
 
- 
+
+
+
+
+
 #z
-
-
-
-# dokladnie sharpe_ratio po prostu stopa zwrotu miesieczna?
-# model CAPM mamy 24 stopy zwrotu czyli mozemy tylko dla ostatniego roku sprawdzac jak dziala CAPM?
-
-
 
 
 
@@ -152,15 +180,15 @@ beta_all <- matrix(c(beta_allX,beta_allY),ncol=2,dimnames = list(NULL,c("beta_al
 #MIESIECZNA STOPA ZWROTU Z OBLIGACJI, PRZEMNOZENIE RAZY LICZBA DNI?
 #ODCHYLENIE MIESIAC OBSERWACJI?
 
-# portfel brzegowy wszystko w jedna sp?lke?
+# portfel brzegowy wszystko w jedna spólke?
 # co z miesiecznymi stopami zwrotu (ostatnia)
 # skroty na gieldzie 2-3-4 literowe
 # portfolio.optim ok, czy liczyc??? na piechote?
-# srednia z 12 miesiecy z 8 sp?lek?
+# srednia z 12 miesiecy z 8 spólek?
 # dlugosc opisu?
 # xtable tabela w latexie
-# wstep, opis sp?lek, macierz korelacji, jak dziala nasza strategia, por?wnanie portfeli, wag
+# wstep, opis spólek, macierz korelacji, jak dziala nasza strategia, porównanie portfeli, wag
 # drowdawn, krzywa efektywnosci, legendy, analiza CAPM, podsumowanie, analiza ryzyka, linie (SML, CML)?
 # tabelki z wagami, zwrotami, ryzyko itp. Dlaczego ostatnia stopa zwrotu jest z 31 grudnia? Wytlumaczyc stopy zwrotu
 # dobranie calego portfela z akcji moze byc trudne (duze koszty transakcji), ale mozemy "kupic" kontrakt
-# sharpe ratio z wsp?lczynnikiem wolnym od ryzyka.Beta w regresji liniowej. Duze zmiany bety- model jest niestabilny.
+# sharpe ratio z wspólczynnikiem wolnym od ryzyka.Beta w regresji liniowej. Duze zmiany bety- model jest niestabilny.
