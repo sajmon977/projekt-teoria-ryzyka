01d381dc2ff791863bd32a56abbaf3b82137a101
przekorny <39132868+przekorny@users.noreply.github.com>
2020-05-14 21:14:13 +0200

    Add files via upload

--- ProjektAdaśSzymon1.R
+++ ProjektAdaśSzymon1.R
@@ -195,7 +195,7 @@ legend(dane_all[dates[22],1],-0.08,legend=c("CAPM", "faktyczna stopa ","zwrotu z
 # Krzywa SML
 bet <- c(0:18)*0.1
 Rm <- stopyzwrotu[1,4] + bet*(eststzwrotugielda[1]-stopyzwrotu[1,4])
-plot(bet,Rm, type = "l",xlim= c(0,2.2),ylim= c(0,0.08),xlab = "beta", ylab = "oczekiwana stopa zwrotu")
+plot(bet,Rm, type = "l",xlim= c(0,2.2),ylim= c(0,0.045),xlab = "beta", ylab = "oczekiwana stopa zwrotu")
 points(beta_all[1,1],mean(rowSums(return_all[1:12,]*wagi_all[1,])),col= rainbow(10)[1],pch=20)
 points(beta_all[1,2],mean(rowSums(return_all[1:12,]*(1/8))),col= rainbow(10)[2],pch=20)
 punktpB <- c()
@@ -203,7 +203,7 @@ for(i in 1:8){
   punktpB <- lm((return_all[1:12,i]-stopa_all[1:12])~(gielda_all[1:12]-stopa_all[1:12]))  
   points(punktpB$coefficients[2],mean(return_all[1:12,i]),col=rainbow(10)[(i+2)],pch=20)
 }
-legend(1.8,0.08,lty =1,y.intersp = 0.8, legend = c("portfelX","portfelY",colnames(return_all)),col=rainbow(10),bty='n',pch=20)
+legend(1.77,0.045,lty =1,y.intersp = 0.8, legend = c("portfelX","portfelY",colnames(return_all)),col=rainbow(10),bty='n',pch=20)
 
 
 
